06fbc9d33c16b9b394af8ca7ee63e0c589cb57b4
naruuu62 <nurilarifin270@gmail.com>
2025-11-26 11:02:00 +0700

    save sementara

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Login — LearnCode</title>
-  <link rel="stylesheet" href="../assets/css/style.css" />
-  <link rel="stylesheet" href="../assets/css/components.css" />
-  <link rel="stylesheet" href="../assets/css/responsive.css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
